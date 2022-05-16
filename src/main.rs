7dbe947e533f2125b99ba7a2c39411a2b49b2dd1
James Holdren <jamesdholdren@gmail.com>
2022-05-16 00:06:56 -0400

    Got imports working

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use serde::Deserialize;
 
 mod templating;
 
+type Fallable<T> = Result<T, ErrMsg>;
+
 // Defining a custom error message
 #[derive(Debug)]
 pub struct ErrMsg(String);
@@ -19,13 +21,23 @@ impl From<std::io::Error> for ErrMsg {
 
 // This generates the blog 'index.html's from the location and manifest
 fn main() {
-    let manifest = get_manifest();
-    for (name, args) in manifest.pages {
-        let file_path =
-            gen_page(&name, &args).expect(&format!("unable to generate file for {}", &name));
+    let manifest = get_manifest().expect("error parsing manifest");
 
-        println!("generated file: {}", file_path);
+    // Parse all reusable pieces
+    let reusables = get_reusables().expect("unable to parse resuables");
+
+    // Generate everything for the `pages` key
+    for (name, args) in manifest.pages {
+        // Load the page and then apply template processing
+        let file_contents = read_file(&args.file).expect("reading in file contents");
+
+        let generated_file_path = gen_page(&name, file_contents, &args, &reusables)
+            .expect(&format!("unable to generate file for {}", &name));
+
+        println!("generated file: {}", generated_file_path);
     }
+
+    // TODO: Generate something using the blog template and data
 }
 
 #[derive(Deserialize, Debug)]
@@ -43,28 +55,59 @@ struct TemplateArgs {
 }
 
 // This decodes the manifest into our struct
-fn get_manifest() -> Manifest {
+fn get_manifest() -> Fallable<Manifest> {
     // File reading
     let mut data = String::new();
-    let mut f = File::open("./page_manifest.json").expect("Unable to open manifest");
-    f.read_to_string(&mut data)
-        .expect("Unable to read manifest to string");
+    let mut f = File::open("./page_manifest.json")?;
+    f.read_to_string(&mut data)?;
 
-    serde_json::from_str(&data).expect("Unable to decode json")
+    let manifest = serde_json::from_str(&data).expect("Unable to decode json");
+    Ok(manifest)
+}
+
+// Parses all files in the reusable directory and stores them in a map
+fn get_reusables() -> Fallable<HashMap<String, String>> {
+    let mut m = HashMap::<String, String>::new();
+    for file in fs::read_dir("./reusable")?
+        .into_iter()
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap().path())
+        .filter(|entry| entry.is_file())
+    {
+        let filename = &file.to_str().unwrap();
+        println!("reading in reusable: {}", &filename);
+
+        let contents = read_file(&filename)?;
+
+        m.insert(
+            file.file_name().unwrap().to_str().unwrap().to_owned(),
+            contents,
+        );
+    }
+
+    Ok(m)
+}
+
+fn read_file(name: &str) -> Fallable<String> {
+    let mut data = String::new();
+    let mut f = File::open(name)?;
+    f.read_to_string(&mut data)?;
+
+    Ok(data)
 }
 
 // Generates an file from the given path name and args
-fn gen_page(name: &String, args: &TemplateArgs) -> Result<String, ErrMsg> {
-    // Read in the template file
-    let mut data = String::new();
-    let mut f = File::open(&args.file)?;
-    f.read_to_string(&mut data)?;
-
+fn gen_page(
+    name: &str,
+    raw_text: String,
+    args: &TemplateArgs,
+    reusables: &HashMap<String, String>,
+) -> Fallable<String> {
     // Replace all placeholders, if not skipping
     let s = if !args.skip_templating {
-        templating::apply_placeholders(data, &args.parameters)?
+        templating::apply_placeholders(raw_text, &args.parameters, reusables)?
     } else {
-        data
+        raw_text
     };
 
     // Save off the file
--- src/templating.rs
+++ src/templating.rs
@@ -14,23 +14,24 @@ use std::collections::HashMap;
 ///
 /// The modified value after all placeholders have been processed. If
 /// a placeholder can't be fulfilled, it returns an error.
-pub fn apply_placeholders(s: String, args: &HashMap<String, String>) -> Result<String, ErrMsg> {
+pub fn apply_placeholders(
+    s: String,
+    args: &HashMap<String, String>,
+    reusables: &HashMap<String, String>,
+) -> Result<String, ErrMsg> {
     lazy_static! {
         // Matches `{{name}}` or `{{language2}}`
-        static ref RE: Regex = Regex::new(r"\{\{([a-zA-Z0-9]+)\}\}").unwrap();
+        static ref RE: Regex = Regex::new(r"\{\{([a-zA-Z0-9:\.]+)\}\}").unwrap();
     }
 
-    // TODO: Get all the matches into a vec so we can iterate backwards
+    // Get all the placeholders into a vec so we can iterate backwards
     // We'll keep track of what was matched, and where it started and stopped
     // so we can replace everything inline for `s`.
-    let matches: Vec<(String, (usize, usize))> = {
+    let matches: Vec<(Placeholder, (usize, usize))> = {
         let mut v = Vec::new();
 
         for mat in RE.find_iter(&s) {
-            v.push((
-                dehandlebar(mat.as_str()).to_string(),
-                (mat.start(), mat.end()),
-            ));
+            v.push((to_placeholder(mat.as_str()), (mat.start(), mat.end())));
         }
 
         v.reverse();
@@ -41,21 +42,28 @@ pub fn apply_placeholders(s: String, args: &HashMap<String, String>) -> Result<S
     let mut s = s;
 
     // Start replacing the placeholder locations with their values in the string
-    for (arg_name, (start, end)) in matches.iter() {
-        // First make sure that the placeholder argument has been provided
-        let arg = match args.get(arg_name) {
-            None => return Err(ErrMsg(format!("did not find argument: {}", arg_name))),
-            Some(value) => value,
-        };
+    for (ph, (start, end)) in matches.iter() {
+        let processed = process_placeholder(ph, args, reusables);
+        if let Err(msg) = processed {
+            return Err(msg);
+        }
 
-        // Replace it in the template
-        s.replace_range(start..end, arg);
+        // // Replace it in the template
+        s.replace_range(start..end, processed.unwrap()); // Safe to unwrap, we just checked if
+                                                         // it was an Err
     }
 
     Ok(s)
 }
 
-/// Takes a handlebars placeholder and gets the name of it
+// All types of placeholder and any information pertinent to how to replace it
+#[derive(Debug)]
+enum Placeholder {
+    Value(String),  // Value from our hash map of values
+    Import(String), // Importing from reusable directory
+}
+
+/// Takes a handlebars placeholder and turns it into a placeholder enum
 ///
 /// # Arguments
 ///
@@ -63,14 +71,50 @@ pub fn apply_placeholders(s: String, args: &HashMap<String, String>) -> Result<S
 ///
 /// # Returns
 ///
-/// The string from inside the placeholder
-///
-/// # Examples
+/// The enum representing the placeholder
 ///
 /// ```
 /// let s = dehandlebar("{{name}}");
 /// assert_eq!("name", s);
 /// ```
-fn dehandlebar(ph: &str) -> &str {
-    ph.trim_start_matches("{{").trim_end_matches("}}")
+fn to_placeholder(ph: &str) -> Placeholder {
+    let inner = ph.trim_start_matches("{{").trim_end_matches("}}");
+
+    if inner.starts_with("reusable:") {
+        let template_name = inner.trim_start_matches("reusable:");
+        return Placeholder::Import(template_name.to_owned());
+    }
+
+    Placeholder::Value(inner.to_owned())
+}
+
+/// Takes in a placeholder and returns it rendered back out
+fn process_placeholder<'a>(
+    ph: &Placeholder,
+    values: &'a HashMap<String, String>,
+    reusables: &'a HashMap<String, String>,
+) -> Result<&'a str, ErrMsg> {
+    if let Placeholder::Value(name) = ph {
+        // If the value is found, just return it. Otherwise it's an error
+        // that the template refers to a value that hasn't been defined
+        return match values.get(name) {
+            None => Err(ErrMsg(format!("did not find named value: {}", name))),
+            Some(matched_value) => Ok(&matched_value),
+        };
+    }
+
+    if let Placeholder::Import(reusable_name) = ph {
+        // If the value is found, just return it. Otherwise it's an error
+        // that the template refers to a reusable that hasn't been created.
+        return match reusables.get(reusable_name) {
+            None => Err(ErrMsg(format!("did not find reusable: {}", reusable_name))),
+            Some(matched_value) => Ok(&matched_value),
+        };
+    }
+
+    // TODO: Add other cases here
+    Err(ErrMsg(format!(
+        "can't process this type at this time: {:?}",
+        ph
+    )))
 }
